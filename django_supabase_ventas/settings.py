91136b1d94fce97f9b6c5fa9c2fab80a41151277
Mario <moreno.jm730@gmail.com>
2025-03-05 13:27:17 -0600

    Configurar Vercel para Django

--- django_supabase_ventas/settings.py
+++ django_supabase_ventas/settings.py
@@ -26,9 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%6@)mc!%q=!@rfoio)^zr)$r+w9iurul^rtd##lmk7$^5b6=kl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'  # False en Vercel
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
